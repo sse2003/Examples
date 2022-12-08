79b06ae920e44e1dd437dfbfb7ab79835b8be796
sse <sse2003@mail.ru>
2022-12-08 20:30:02 +0300

    Интересный случай с сортировкой и выборкой уникальных значений из обычной и parallel stream

---
+++ src/main/java/org/sse/examples/stream/TestSortableDistinctStream.java
@@ -0,0 +1,68 @@
+package org.sse.examples.stream;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+import java.util.List;
+
+// Run with VM Options: -ea
+public class TestSortableDistinctStream
+{
+    static List<Integer> SOURCE = List.of(11, 3, 2, 5, 4, 1, 14, 12, 13, 11);
+
+    public static void main(String args[])
+    {
+        testStream(true);
+        testStream(false);
+    }
+
+    static void testStream(boolean isParallel)
+    {
+        System.out.println("Starting test, is parallel: " + isParallel);
+
+        var stream = SOURCE.stream();
+        if (isParallel) stream = stream.parallel();
+
+        var result = stream
+                .map(v -> new Wrapper(v % 10, v))
+                .peek(v -> System.out.println("origin: " + v))
+                .sorted()
+                .peek(v -> System.out.println("sorted: " + v))
+                .distinct()
+                .peek(v -> System.out.println("distinct: " + v))
+                .toList();
+
+        System.out.println("result: " + result);
+        System.out.println();
+
+        assert (result.size() == 5);
+    }
+
+    @Value
+    @AllArgsConstructor
+    static class Wrapper implements Comparable<Wrapper>
+    {
+        // Key
+        Integer value;
+
+        // Priority with ASC sorting
+        @EqualsAndHashCode.Exclude
+        Integer priority;
+
+        @Override
+        public int compareTo(Wrapper o)
+        {
+            return priority.compareTo(o.priority); // ASC
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Wrapper{" +
+                    "value=" + value +
+                    ", priority=" + priority +
+                    '}';
+        }
+    }
+}
